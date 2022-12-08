e7762792d1e13c1bb6aa6a684cd7bc8fb896c2ed
MarialuizaLeao <maria_luiza1598@hotmail.com>
2022-12-08 13:46:39 -0300

    formulação do grafo

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import random as rd
+
+def Manhattan(x, y):
+    matriz = np.full((len(x), len(y)), 0, dtype=int)
+    for i in range(len(x)):
+        for j  in range(len(y)):
+            if (i == j):
+                matriz[i][j] = -1
+            else:
+                dist = abs(x[i] - x[j]) + abs(y[i] - y[j])
+        matriz[i][j] = dist
+        matriz[j][i] = dist
+    return matriz
+
+def Euclides(x, y):
+    matriz = np.full((len(x), len(y)), 0, dtype=int)
+    for i in range(len(x)):
+        for j  in range(len(y)):
+            if (i == j):
+                matriz[i][j] = -1
+            else:
+                dist = np.sqrt((x[i] - x[j])**2 + (y[i] - y[j])**2) 
+                dist = abs(dist)
+        matriz[i][j] = dist
+        matriz[j][i] = dist
+    return matriz
+
+def geradorPontos(N):
+    xCoordenadas = np.random.random_integers(0, 400, size = N)
+    yCoordenadas = np.random.random_integers(0, 400, size = N)
+    x = np.unique(xCoordenadas)
+    while (len(xCoordenadas) < N):
+        x = np.append(x, np.random.random_integers(0, 40))
+        x = np.unique(xCoordenadas)
+    return xCoordenadas, yCoordenadas
